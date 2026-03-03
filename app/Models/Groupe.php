a2fbe8efce0466871b93572b2e1f1e415f4bd811
Mohamed Bendada <bendada.mohamed00@gmail.com>
2026-03-03 23:20:06 +0000

    feat: ajouter hook saving pour valider cohérence option/filiere

--- app/Models/Groupe.php
+++ app/Models/Groupe.php
@@ -3,6 +3,7 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use App\Models\Option;
 
 class Groupe extends Model
 {
@@ -32,5 +33,21 @@ class Groupe extends Model
 
     public function filiere(){
         return $this->belongsTo(Filiere::class);
+    }
+
+    protected static function boot()
+    {
+        parent::boot();
+
+        static::saving(function ($groupe) {
+            if ($groupe->option_id) {
+                $option = Option::find($groupe->option_id);
+                if ($option->filiere_id !== $groupe->filiere_id) {
+                    throw new \InvalidArgumentException(
+                        "L'option sélectionnée n'appartient pas à la filière choisie."
+                    );
+                }
+            }
+        });
     }
 }
