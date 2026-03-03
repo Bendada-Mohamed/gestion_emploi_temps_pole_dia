74b206c46356590d9f13265dfcb57d5ae19f667a
Mohamed Bendada <bendada.mohamed00@gmail.com>
2026-03-03 22:28:17 +0000

    feat: ajouter UpdateSeanceRequest avec validation date, format heure et clés étrangères

---
+++ app/Http/Requests/UpdateSeanceRequest.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+class UpdateSeanceRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    public function authorize(): bool
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array<mixed>|string>
+     */
+    public function rules(): array
+    {
+        return [
+            'groupe_id' => 'required|exists:groupes,id',
+            'salle_id' => 'required|exists:salles,id',
+            'formateur_id' => 'required|exists:formateurs,id',
+            'date' => 'required|date',
+            'heure_debut' => 'required|date_format:H:i',
+            'heure_fin' => 'required|date_format:H:i|after:heure_debut',
+        ];
+    }
+}
