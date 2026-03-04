f48fc6d59e505121ec1ba878790159e7d7b62c7a
Mohamed Bendada <bendada.mohamed00@gmail.com>
2026-03-04 04:32:53 +0000

    feat: enregistrer les middlewares IsAdmin, IsFormateur, IsStagiaire

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -11,7 +11,11 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware): void {
-        //
+        $middleware->alias([
+        'admin'     => \App\Http\Middleware\IsAdmin::class,
+        'formateur' => \App\Http\Middleware\IsFormateur::class,
+        'stagiaire' => \App\Http\Middleware\IsStagiaire::class,
+        ]);
     })
     ->withExceptions(function (Exceptions $exceptions): void {
         //
